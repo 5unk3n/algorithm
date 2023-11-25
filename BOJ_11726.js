dc3c3ef6a4fcec73cfd68476406808f310e1dd23
Seongeun Lee <tjsrms1227@gmail.com>
2023-11-25 22:32:58 +0900

    BOJ_11726

---
+++ BOJ_11726.js
@@ -0,0 +1,12 @@
+const input = require('fs')
+  .readFileSync(process.platform == 'linux' ? 'dev/stdin' : 'test/test.txt')
+  .toString()
+  .trim();
+
+const dp = [1, 2];
+
+for (let i = 2; i < input; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+console.log(dp[input - 1]);
