d916aa3821f1136c3a4e4d1d1b4f702893fb723a
이선근 <tjsrms1227@gmail.com>
2023-12-05 15:42:11 +0900

    Update 히오스 프로게이머.js

--- 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.js
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.js
@@ -13,20 +13,12 @@ const levels = input
 let min = 1;
 let max = levels[N - 1] + K;
 
-if (N === 1) {
-  console.log(levels[0] + K);
-  return;
-}
-
 while (min <= max) {
   const middle = Math.floor((min + max) / 2);
   const T = levels.reduce(
     (acc, cur) => (cur > middle ? acc : acc + middle - cur),
     0
   );
-  // middle이 목표 레벨
-  // T가 뭐냐? T는 목표레벨까지 올린 레벨의 합 => K보다 높을 수 없음
-  // 같거나 낮으면 맥스 내려
 
   if (T <= K) {
     min = middle + 1;
@@ -36,11 +28,3 @@ while (min <= max) {
 }
 
 console.log(max);
-
-// 10 20 15
-
-// 0          20
-//      11    20
-//       16   20
-//       1617
-//         17       => 9
