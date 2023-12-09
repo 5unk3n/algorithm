8059b51c609f1aec2bfe356ba53a674596e077f0
이선근 <tjsrms1227@gmail.com>
2023-12-09 11:35:21 +0900

    [Silver I] Title: IOIOI, Time: 204 ms, Memory: 12072 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -0,0 +1,49 @@
+const input = require('fs')
+  .readFileSync(process.platform == 'linux' ? 'dev/stdin' : 'test/test.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+const N = +input[0];
+const M = +input[1];
+const S = input[2];
+
+let result = 0;
+
+let startIdx = null;
+let endIdx = null;
+
+let prevItem = null;
+let currItem = null;
+
+for (let i = 0; i < M; i++) {
+  currItem = S[i];
+
+  if (currItem === 'I') {
+    if (startIdx === null) {
+      startIdx = i;
+    } else if (prevItem === 'O') {
+      endIdx = i;
+    }
+  }
+
+  if (prevItem === currItem || i === M - 1) {
+    const indexDiff = endIdx - startIdx;
+    const count = Math.floor(indexDiff / 2);
+
+    if (count >= N) {
+      result += count - N + 1;
+    }
+
+    if (currItem === 'I') {
+      startIdx = i - 1;
+    } else {
+      startIdx = null;
+      endIdx = null;
+    }
+  }
+
+  prevItem = S[i];
+}
+
+console.log(result);
