010a04e11d3a077981348d036fdbc9f3ba9c4cd3
이선근 <tjsrms1227@gmail.com>
2023-12-13 10:28:26 +0900

    [Silver III] Title: N과 M (5), Time: 232 ms, Memory: 22880 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -0,0 +1,36 @@
+const input = require('fs')
+  .readFileSync(process.platform == 'linux' ? 'dev/stdin' : 'test/test.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+const [N, M] = input[0].split(' ').map(Number);
+const arr = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+
+const result = [];
+
+const combination = [];
+const visited = [];
+
+const solution = (idx) => {
+  if (idx === M) {
+    result.push(combination.join(' '));
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    if (visited[i]) continue;
+
+    visited[i] = true;
+    combination[idx] = arr[i];
+    solution(idx + 1);
+    visited[i] = false;
+  }
+};
+
+solution(0);
+
+console.log(result.join('\n'));
