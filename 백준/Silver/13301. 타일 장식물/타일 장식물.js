d316db67ecdb0fb94eadc45ead2f024f4c9890aa
이선근 <tjsrms1227@gmail.com>
2023-12-24 23:00:55 +0900

    [Silver V] Title: 타일 장식물, Time: 128 ms, Memory: 9332 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13301. 타일 장식물/타일 장식물.js
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.js
@@ -7,11 +7,12 @@ const input = require('fs')
 const N = +input[0];
 
 const dp = Array.from({ length: N + 1 }, () => 0);
-dp[1] = 1;
-dp[2] = 1;
+dp[0] = 0n;
+dp[1] = 1n;
+dp[2] = 1n;
 
 for (let i = 3; i <= N; i++) {
   dp[i] = dp[i - 1] + dp[i - 2];
 }
 
-console.log(dp[N] * 4 + dp[N - 1] * 2);
+console.log((dp[N] * 4n + dp[N - 1] * 2n).toString());
