4fb78848c7d1102c9d3b61a24979a853f993d913
이선근 <tjsrms1227@gmail.com>
2023-12-28 22:23:16 +0900

    [Silver III] Title: Four Squares, Time: 236 ms, Memory: 10808 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.js
@@ -0,0 +1,15 @@
+const input = require('fs')
+  .readFileSync(process.platform == 'linux' ? 'dev/stdin' : 'test/test.txt')
+  .toString()
+  .trim();
+
+const dp = Array.from({ length: +input + 1 }, () => Infinity);
+dp[0] = 0;
+
+for (let i = 1; i <= input; i++) {
+  for (let j = 1; j * j <= i; j++) {
+    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+  }
+}
+
+console.log(dp[input]);
