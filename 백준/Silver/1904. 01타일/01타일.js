dda8f53b38e7316039f5d7a2d7a7a6d1206440e0
이선근 <tjsrms1227@gmail.com>
2024-02-14 10:53:34 +0900

    [Silver III] Title: 01타일, Time: 260 ms, Memory: 17488 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.js
@@ -0,0 +1,14 @@
+const N = +require('fs')
+  .readFileSync(process.platform == 'linux' ? 'dev/stdin' : 'test/test.txt')
+  .toString()
+  .trim();
+
+const dp = Array.from({ length: N + 1 }, () => 0);
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= N; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+}
+
+console.log(dp[N]);
